42f41f8d9560eb48b3459c2a0305829d59e7c3e3
oolwinds <antihope4peace@gmail.com>
2023-05-07 19:30:18 +0800

    fixed price not changing when different values are inputted

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scaler.fit_transform(np.array(data).reshape(1,-1))
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("home.html", prediction_text="The price of the house is estimated to be ${:,.0f}.".format(output))
+    return render_template("home.html", prediction_text="The price of the house is estimated to be ${:,.0f}".format(output))
 
 
 if __name__=="__main__":
