95d6cf6831cae7a50b8abea486da8a65e5740f3f
oolwinds <antihope4peace@gmail.com>
2023-05-07 18:13:07 +0800

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# housingprediction
+# House Price Prediction
+
+
+Create a new environment for the project
+
+```
+conda create -p venv python==3.8 -y
+```
---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+import pickle
+from flask import Flask, app,jsonify, url_for,render_template,redirect,flash,session,escape,request
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+#Load the model
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.fit_transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The price of the house is estimated to be ${:,.0f}.".format(output))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
